2a9ead512a6e72797c35b4678434c006f4dd3bf5
gemmadifederico <gemmadifederico@studenti.unicam.it>
2019-06-07 17:59:48 +0200

    Modified the view as a table

--- Kitchen Management System/src/main/java/com/gui/OrderStatus.java
+++ Kitchen Management System/src/main/java/com/gui/OrderStatus.java
@@ -1,5 +1,8 @@
 package com.gui;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Component;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -12,6 +15,8 @@ import java.util.Observable;
 import java.util.Observer;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableCellRenderer;
+import javax.swing.table.DefaultTableModel;
 
 import com.bin.Order;
 
@@ -24,8 +29,8 @@ public class OrderStatus implements Observer {
 	
 	public OrderStatus() {
 		this.orderList = new ArrayList<Order>();
-		this.orderPanel = new JPanel(new GridLayout(0,2));
-		this.orderPanel.setBorder(BorderFactory.createEmptyBorder(2,2,2,2));
+		this.orderPanel = new JPanel(new BorderLayout());
+		//this.orderPanel.setBorder(BorderFactory.createEmptyBorder(2,2,2,2));
 		
 		this.init();
 	}
@@ -106,7 +111,7 @@ public class OrderStatus implements Observer {
 			frame.setTitle("Order Status");
 			frame.setSize(800,600);
 			frame.setDefaultCloseOperation(3);
-			DynamicFramePosition.setLocationToRight(frame);
+			//DynamicFramePosition.setLocationToRight(frame);
 		    frame.setVisible(true);
 			writeOrders();
 			frame.setContentPane(orderPanel);
@@ -120,43 +125,43 @@ public class OrderStatus implements Observer {
 	private void writeOrders() {
 		this.orderPanel.setVisible(false);
 		this.orderPanel.removeAll();
-		for(Order order : this.orderList) {
-			JLabel orderLabel;
-			if(order.getID() == this.lastOrderChanged) {
-				orderLabel = new JLabel(
-						"<html>"+
-							"<font color='green'>"+
-								"ID: "+order.getID() +"<br/>"+
-								"Type: "+order.getType() +"<br/>"+
-								"Waiter ID: "+order.getWaiterID() +"<br/>"+
-								"Priority: "+order.getPriority() +"<br/>"+
-								"Status: "+order.getStatus() +"<br/>"+
-								"Drinks: "+order.getDrink()+"<br/>"+
-								"Starter: "+order.getStarter()+"<br/>"+
-								"First Course: "+order.getFirstCourse()+"<br/>"+
-								"Second Course: "+order.getSecondCourse()+"<br/>"+
-								"Dessert: "+order.getDessert()+
-							"</font>"+
-						"</html>"
-				);
-			} else {
-				orderLabel = new JLabel(
-						"<html>"+
-							"ID: "+order.getID() +"<br/>"+
-							"Type: "+order.getType() +"<br/>"+
-							"Waiter ID: "+order.getWaiterID() +"<br/>"+
-							"Priority: "+order.getPriority() +"<br/>"+
-							"Status: "+order.getStatus() +"<br/>"+
-							"Drinks: "+order.getDrink()+"<br/>"+
-							"Starter: "+order.getStarter()+"<br/>"+
-							"First Course: "+order.getFirstCourse()+"<br/>"+
-							"Second Course: "+order.getSecondCourse()+"<br/>"+
-							"Dessert: "+order.getDessert()+
-						"</html>"
-				);
-			}
-			this.orderPanel.add(orderLabel);
+		 Object[][] rowData = {};
+		 Object[] columnNames = { "ID", "Type", "Waiter ID", "Priority", "Status", "Drinks", "Starter", "First Course", "Second Course", "Dessert" };
+		DefaultTableModel tableModel = new DefaultTableModel(rowData, columnNames);
+		JTable table = new JTable(tableModel) {
+			@Override public boolean isCellEditable(int arg0, int arg1) {
+				return false; 
+				}
+		};
+		table.setFillsViewportHeight(true);
+		for(Order order : this.orderList) {			
+				Object[] objs = {order.getID(), order.getType(), order.getWaiterID(), order.getPriority(), order.getStatus(),
+						order.getDrink().getContent(), order.getStarter().getContent(), order.getFirstCourse().getContent(),
+						order.getSecondCourse(), order.getDessert().getContent()};
+				tableModel.addRow(objs);
+
+				table.setDefaultRenderer(Object.class, new DefaultTableCellRenderer(){
+				    @Override
+				    public Component getTableCellRendererComponent(JTable table,
+				            Object value, boolean isSelected, boolean hasFocus, int row, int col) {
+
+				        super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, col);
+
+				        Integer status = (Integer)table.getModel().getValueAt(row,0);
+				        if (lastOrderChanged == status) {
+				            setBackground(Color.GREEN);
+				        } else {
+				            setBackground(table.getBackground());
+				            setForeground(table.getForeground());
+				        }       
+				        return this;
+				    }   
+				});
 		}
+		
+		
+		this.orderPanel.add(new JScrollPane(table), BorderLayout.CENTER);
+		//this.orderPanel.add(table);
 		this.orderPanel.revalidate();
 		this.orderPanel.setVisible(true);
 	}
